320536d3b896f01141d09a4986a2fb70862e9b22
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 20:28:51 +0100

    Rotate clockwise the current buffers

--- init.el
+++ init.el
@@ -22,6 +22,27 @@
 (require 'auto-complete)
 (require 'buffer-move)
 
+(defun rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond
+   ((not (> (count-windows) 1)) (message "You can't rotate a single window!"))
+   (t
+    (let ((i 1)
+          (num-windows (count-windows)))
+      (while  (< i num-windows)
+        (let* ((w1 (elt (window-list) i))
+               (w2 (elt (window-list) (+ (% i num-windows) 1)))
+               (b1 (window-buffer w1))
+               (b2 (window-buffer w2))
+               (s1 (window-start w1))
+               (s2 (window-start w2)))
+          (set-window-buffer w1  b2)
+          (set-window-buffer w2 b1)
+          (set-window-start w1 s2)
+          (set-window-start w2 s1)
+          (setq i (1+ i))))))))
+
 (require 'projectile)
 (projectile-global-mode)
 
@@ -170,7 +191,7 @@
 ;; (global-set-key (kbd "C-c d f") 'diff-buffer-with-file)
 
 ;;window and buffer movement
-(global-set-key (kbd "C-c w s") 'swap-windows)
+;;(global-set-key (kbd "C-c w s") 'swap-windows)
 (global-set-key (kbd "C-c w r") 'rotate-windows)
 (global-set-key (kbd "C-c w p") 'buf-move-up)
 (global-set-key (kbd "C-c w n") 'buf-move-down)
