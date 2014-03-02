9070d05f314610483bf5c7463a2f31ad5780848e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-02 09:25:34 +0100

    Move function

--- init.el
+++ init.el
@@ -22,27 +22,6 @@
 (require 'auto-complete)
 (require 'buffer-move)
 
-(defun rotate-windows ()
-  "Rotate your windows"
-  (interactive)
-  (cond
-   ((not (> (count-windows) 1)) (message "You can't rotate a single window!"))
-   (t
-    (let ((i 1)
-          (num-windows (count-windows)))
-      (while  (< i num-windows)
-        (let* ((w1 (elt (window-list) i))
-               (w2 (elt (window-list) (+ (% i num-windows) 1)))
-               (b1 (window-buffer w1))
-               (b2 (window-buffer w2))
-               (s1 (window-start w1))
-               (s2 (window-start w2)))
-          (set-window-buffer w1  b2)
-          (set-window-buffer w2 b1)
-          (set-window-start w1 s2)
-          (set-window-start w2 s1)
-          (setq i (1+ i))))))))
-
 (require 'projectile)
 (projectile-global-mode)
 
@@ -80,6 +59,27 @@
 (require 'etags)
 (require 'smartscan)
 (global-smartscan-mode)
+
+(defun rotate-windows ()
+  "Rotate your windows"
+  (interactive)
+  (cond
+   ((not (> (count-windows) 1)) (message "You can't rotate a single window!"))
+   (t
+    (let ((i 1)
+          (num-windows (count-windows)))
+      (while  (< i num-windows)
+        (let* ((w1 (elt (window-list) i))
+               (w2 (elt (window-list) (+ (% i num-windows) 1)))
+               (b1 (window-buffer w1))
+               (b2 (window-buffer w2))
+               (s1 (window-start w1))
+               (s2 (window-start w2)))
+          (set-window-buffer w1  b2)
+          (set-window-buffer w2 b1)
+          (set-window-start w1 s2)
+          (set-window-start w2 s1)
+          (setq i (1+ i))))))))
 
 (defun goto-line-with-feedback ()
   "Show line numbers temporarily, while prompting for the line number input"
