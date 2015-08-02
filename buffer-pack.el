e5faf10b68ea429c85d791a16d3ca74bf41817ac
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-02 11:23:47 +0200

    Add helper functions to fight against typos
    source: http://endlessparentheses.com/ispell-and-abbrev-the-perfect-auto-correct.html

--- buffer-pack.el
+++ buffer-pack.el
@@ -300,6 +300,8 @@ Otherwise, we go inside a terminal."
 
 (setq load-prefer-newer t)
 
+;; fix typos
+
 (defun dcaps-to-scaps ()
   "Convert word in DOuble CApitals to Single Capitals."
   (interactive)
@@ -322,6 +324,33 @@ Single Capitals as you type."
     (remove-hook 'post-self-insert-hook #'dcaps-to-scaps 'local)))
 
 (add-hook 'text-mode-hook #'dubcaps-mode)
+
+;; fix typos
+
+(define-key ctl-x-map "\C-i" #'endless/ispell-word-then-abbrev)
+
+(defun endless/ispell-word-then-abbrev (p)
+  "Call `ispell-word', then create an abbrev for it.
+With prefix P, create local abbrev.
+Otherwise it will be global."
+  (interactive "P")
+  (let ((bef (downcase (or (thing-at-point 'word)
+                           "")))
+        aft)
+    (call-interactively 'ispell-word)
+    (setq aft (downcase
+               (or (thing-at-point 'word) "")))
+    (unless (or (string= aft bef)
+                (string= aft "")
+                (string= bef ""))
+      (message "\"%s\" now expands to \"%s\" %sally"
+               bef aft (if p "loc" "glob"))
+      (define-abbrev
+        (if p local-abbrev-table global-abbrev-table)
+        bef aft))))
+
+(setq save-abbrevs t)
+(setq-default abbrev-mode t)
 
 (provide 'buffer-pack)
 ;;; buffer-pack.el ends here
